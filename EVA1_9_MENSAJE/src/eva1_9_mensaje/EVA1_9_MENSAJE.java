730a590830e85a08a419e5dab1293caecd78c537
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-02-21 12:51:55 -0600

    Add files via upload

---
+++ EVA1_9_MENSAJE/src/eva1_9_mensaje/EVA1_9_MENSAJE.java
@@ -0,0 +1,14 @@
+package eva1_9_mensaje;
+
+public class EVA1_9_MENSAJE {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        nPrintLn("HOLA",5);//llamada funcion
+    }
+    public static void nPrintLn(String message, int num){
+    for (int i = 0; i < num; i++) {
+        System.out.println(message);
+        } 
+    }
+}
