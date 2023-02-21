328193f01a8eb80da70db157b9aa9e9debeab92a
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-02-21 12:40:23 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,15 @@
+package eva1_4_television;
+
+public class EVA1_4_TELEVISION {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tele1 tv1 = new Tele1(); 
+        tv1.cambiarPower();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+        tv1.bajarCanal();
+    }
+    
+}
