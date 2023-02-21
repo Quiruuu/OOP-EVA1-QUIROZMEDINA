bb5873379f778fe913e60256d354f8fee1bf3548
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-02-21 12:50:55 -0600

    Add files via upload

---
+++ EVA1_8_LLAMADA_FUNCION/src/eva1_8_llamada_funcion/EVA1_8_LLAMADA_FUNCION.java
@@ -0,0 +1,24 @@
+package eva1_8_llamada_funcion;
+
+public class EVA1_8_LLAMADA_FUNCION {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("INICIA main()");
+    A();    
+    System.out.println("TERMINA main()");
+    }
+    
+    public static void A(){
+    System.out.println("INICIA A()");
+    B();    
+    System.out.println("TERMINA A()");
+    }
+    
+    public static void B(){
+    System.out.println("INICIA B()");
+        
+    System.out.println("TERMINA B()");
+    }
+    
+}
