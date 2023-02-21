22a75cc981ffbbf274f101341f65f24b84110cd4
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-02-21 12:50:32 -0600

    Add files via upload

---
+++ EVA1_7_METODOS_1/src/eva1_7_metodos_1/EVA1_7_METODOS_1.java
@@ -0,0 +1,12 @@
+package eva1_7_metodos_1;
+
+public class EVA1_7_METODOS_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        myMethod();
+    }
+    public static void myMethod(){
+    System.out.println("You Have called me! My name is PacoElTaco");
+    }
+}
