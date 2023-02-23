c5c404c4bf0eca4dfb7c4ba83069636906e5834f
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-02-23 12:46:06 -0600

    Add files via upload

---
+++ EVA1_15_SOBRECARGA/src/eva1_15_sobrecarga/EVA1_15_SOBRECARGA.java
@@ -0,0 +1,17 @@
+package eva1_15_sobrecarga;
+
+public class EVA1_15_SOBRECARGA {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD!!");
+        System.out.println(100);
+        System.out.println(true);
+        System.out.println(args); 
+    }
+    public static void algo(){
+    
+    }
+    public static void algo(int x){
+    
+    }
+}
