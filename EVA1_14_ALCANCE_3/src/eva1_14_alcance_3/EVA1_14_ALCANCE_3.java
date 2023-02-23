8375569803f2f7cee3426e2f6015c24f7ebe66db
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-02-23 12:39:12 -0600

    Add files via upload

---
+++ EVA1_14_ALCANCE_3/src/eva1_14_alcance_3/EVA1_14_ALCANCE_3.java
@@ -0,0 +1,26 @@
+package eva1_14_alcance_3;
+
+public class EVA1_14_ALCANCE_3 {
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println(x);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(x);
+            System.out.println(i);
+            if(i == 5){
+                System.out.println(x);
+            } 
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+ public static void Algo(int x){
+     
+    }
+
+public static void Algo2(int x){
+    
+    }
+
+}
