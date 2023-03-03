bdffa95b065585bee507a149bf9520f3f82798d3
Quiru <94187467+Quiruuu@users.noreply.github.com>
2023-03-03 15:50:37 -0600

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,11 @@
+package eva1_12_alcance;
+public class EVA1_12_ALCANCE {
+    public static void main(String[] args) {
+        
+        System.out.println(x);//NO EXISTE LA X PORQUE ESTA DECLARADA DESPUES DE LA INSTRUCCION
+        System.out.println(args);//ARGS ESTA DECLARADA ANTES DE LA INSTRUCCION
+        int x = 100;
+        System.out.println(x);
+    }
+    
+}
